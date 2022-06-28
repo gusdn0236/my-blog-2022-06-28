d326b0bf0a636a3846168b5ce0d9e4066a676e51
hongsikcho <jjdd9758@naver.com>
2022-06-28 09:27:48 +0900

    test-space

---
+++ src/pages/test-space.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+function TestSpacePage() {
+    return <main>
+        <title>테스트페이지</title>
+        <h1>테스트 스페이스 페이지 입니다.</h1>
+    </main>
+}
+  
+export default TestSpacePage;
---
+++ src/pages/test.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+function TestPage() {
+    return <main>
+        <title>테스트페이지</title>
+        <h1>테스트 페이지 입니다.</h1>
+    </main>
+}
+  
+export default TestPage;
